7a8c5d560c022ca5a04a345e4bc9627727f4c5f6
DuanYu-2016 <duan_yu_2016@outlook.com>
2025-09-06 09:01:44 +0800

    20250906：尝试内置Python+MaaFW

--- install.py
+++ install.py
@@ -75,6 +75,10 @@ def install_agent():
         install_path / "agent",
         dirs_exist_ok=True,
     )
+    shutil.copytree(
+        working_dir / "python",
+        install_path /,
+    )
 
 if __name__ == "__main__":
     install_deps()
